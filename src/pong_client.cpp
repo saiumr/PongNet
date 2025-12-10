b40afc83d2aa1d1ceda84b56ad523872c357ff55
saiumr <colaloveanan@gmail.com>
2025-12-10 15:35:17 +0800

    change game arch.

---
+++ src/pong_client.cpp
@@ -0,0 +1,11 @@
+#include <SDL3/SDL.h>
+#include <SDL3_net/SDL_net.h>
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_EVENTS);
+    NET_Init();
+    SDL_Log("hello pont net!");
+    NET_Quit();
+    SDL_Quit();
+    return 0;
+}
